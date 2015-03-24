5b0ed47a723f244b92d32bc807d770d4de0342b3
lokesh-webonise <lokesh@weboniselab.com>
2015-03-24 11:39:25 +0530

    changes in repo clone path

--- drf/recipes/default.rb
+++ drf/recipes/default.rb
@@ -16,7 +16,7 @@ script "clone repo" do
   interpreter "bash"
   flags "-x"
   user "root"
-  cwd "/home/vagrant"
+  cwd "/vagrant"
   code <<-EOH
   /usr/bin/git svn clone svn+ssh://deploy@drf.prometheusdata.com/home/svn/weboniselab/#{node['svn']['reponame']}
   sed 's/deploy/#{node['svn']['username']}' /home/vagrant/#{node['svn']['reponame']}/.git/config
