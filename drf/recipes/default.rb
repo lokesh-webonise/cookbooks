62bf7efa5af630fe0285cb94c8fb73487165364b
lokesh <lokesh@devops.(none)>
2015-04-21 11:56:22 +0530

    cookbook name changeed in metadata.rb

--- drf/recipes/default.rb
+++ drf/recipes/default.rb
@@ -19,6 +19,6 @@ script "clone repo" do
   cwd "/vagrant"
   code <<-EOH
   /usr/bin/git svn clone svn+ssh://deploy@drf.prometheusdata.com/home/svn/weboniselab/#{node['svn']['reponame']}
-  sed 's/deploy/#{node['svn']['username']}' /home/vagrant/#{node['svn']['reponame']}/.git/config
+  sed -i 's/deploy/#{node['svn']['username']}/g' /home/vagrant/#{node['svn']['reponame']}/.git/config
   EOH
 end
