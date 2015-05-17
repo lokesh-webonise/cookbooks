433f4a7a190bc0b0b70ce4bbfc11d1e25990dc36
lokesh <lokesh@lokeshs-MacBook-Pro.local>
2015-05-17 22:49:02 +0530

    repo name changed to weboniselab

--- drf/recipes/default.rb
+++ drf/recipes/default.rb
@@ -18,7 +18,7 @@ script "clone repo" do
   user "root"
   cwd "/vagrant"
   code <<-EOH
-  /usr/bin/git svn clone svn+ssh://deploy@drf.prometheusdata.com/home/svn/weboniselab/#{node['svn']['reponame']}/branches/hotfix_StaticFlagAuthService
-  /bin/sed -i 's/deploy/#{node['svn']['username']}/g' /vagrant/#{node['svn']['reponame']}/.git/config
+  /usr/bin/git svn clone svn+ssh://deploy@drf.prometheusdata.com/home/svn/weboniselab
+  /bin/sed -i 's/deploy/#{node['svn']['username']}/g' /vagrant/weboniselab/.git/config
   EOH
 end
